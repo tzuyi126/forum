38dbf40131c8825825bfb3a53e391a74eef0d14c
106062123 <belle880126@gmail.com>
2019-04-27 13:34:27 +0800

    update 13.34 4/27

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAeM7BusMmOY2Ae8PCMcLKDrlmxh7y8dTs",
+    authDomain: "forummmm-6bc9d.firebaseapp.com",
+    databaseURL: "https://forummmm-6bc9d.firebaseio.com",
+    projectId: "forummmm-6bc9d",
+    storageBucket: "forummmm-6bc9d.appspot.com",
+    messagingSenderId: "130453301773"
+  };
+  firebase.initializeApp(config);
